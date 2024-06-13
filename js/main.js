d807a7edf373f8da2d272db66f6a9fdcd11a8261
rupinderk14 <102467626+rupinderk14@users.noreply.github.com>
2024-06-13 22:58:36 +0530

    other folder

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let navlinkheight = document.getElementById('navlink');
+function openbtn(){
+    if(navlinkheight.style.width = '0'){
+        navlinkheight.style.width = '70%';
+    }
+}
+function closebtn(){
+    if(navlinkheight.style.width = '70%'){
+        navlinkheight.style.width = '0';
+    }
+}
